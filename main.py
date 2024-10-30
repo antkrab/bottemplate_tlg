270efe81f0d33f9208aae17ba7717c9f6aaa1d4f
Hanazono <intuch68@gmail.com>
2024-10-30 13:50:34 +0700

    add env parameter

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, filters,  MessageHandler
 
+api_key = os.getenv('API_KEY')
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
@@ -12,7 +14,7 @@ async def echo(update: Update, context) -> None:
     print(f"Message from {user.first_name} in group {chat_id}: {text}")
     await update.message.reply_text(update.message.text)
 
-app = ApplicationBuilder().token("<your api key>").build() # build bot by api key
+app = ApplicationBuilder().token(api_key).build() # build bot by api key
 
 app.add_handler(CommandHandler("hello", hello)) # command /hello -> callback = hello
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo)) #any word -> callback = echo
